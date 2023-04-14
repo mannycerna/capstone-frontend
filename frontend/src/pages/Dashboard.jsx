7698fd27ad179f068c37a8750cc39290ef0e4d60
mannycerna <manuel.cerna@codeimmersives.com>
2023-04-14 14:51:44 -0500

    edit dashboard'

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,19 @@
+import {useEffect} from 'react';
+import {useNaviage} from 'react-router-dom';
+import {useSelector} from 'react-redux'
+
 function Dashboard() {
+   const navigate = useNavigate()
+
+   const {user} = useSelector((state)  => state.auth)
+
+   useEffect(() => {
+      if(!user) {
+         navigate)
+      }
+   }, [user, navigate] )
+
    return (<div>Dashboard</div>)
 }
+
 export default Dashboard
